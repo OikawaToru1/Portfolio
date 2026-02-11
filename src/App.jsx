a2f6d698ea36dd75bd8f912a9ad4369a87a3226c
Aniket <aniketadhikari2@gmail.com>
2026-02-11 20:52:36 +0545

    changes in ui and added project box

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { GoArrowRight } from "react-icons/go";
+import ProjectBox from './Components/ProjectBox';
+import fuzzychat from '../src/assets/project-images/fuzzychat.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = false
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className = {`flex  h-screen w-full overflow-hidden ${theme? "bg-[#0a0a0a]" : "bg-slate-50"}`}>
+    <aside className={` hidden md:flex flex-col shrink-0 w-64 lg:w-72 h-full border-r border-gray-100 transition-all duration-300`}>
+      <ul className=' '>
+        <li>
+          About
+        </li>
+        <li>
+          Projects
+        </li>
+        <li>
+          Contact
+        </li>
+      </ul>
+    </aside>
+    
+    <main className='flex-1 flex flex-col  w-full h-full overflow-hidden relative'>
+      <div className='flex-1  overflow-y-auto custom-scrollbar '>
+        <div  className='w-full  max-w-2xl px-12 lg:px-24 h-102 flex flex-col gap-8 justify-end'>
+         <div>
+          <h1 className='md:text-2xl text-xl font-semibold md:font-bold'>Hi, I'm Aniket</h1>
+          <h3 className='md:text-xl text-lg text-gray-500 '>Final-year Computer Engineering student</h3>
+          <p className='text-lg my-10'>
+             I enjoy building clean, responsive websites and learning modern technologies to create meaningful digital experiences.
+          </p>
+          <span className='flex items-center gap-1 text-lg hover:bg-blue-400 hover:cursor-pointer w-40 '>
+            Learn more
+            <GoArrowRight />
+          </span>
+         </div>
+        </div>
+
+      <div  className='w-full max-w-2xl px-12 lg:px-24 h-80 flex flex-col gap-8 mt-10 border-t  border-gray-200  '>
+         <div className='mt-10'>
+          <h1 className='md:text-xl text-xl font-semibold md:font-bold'>Personal Projects</h1>
+          
+          <p className='text-lg my-10'>
+             I enjoy building clean, responsive websites and learning modern technologies to create meaningful digital experiences.
+          </p>
+
+         </div>
+        </div>
+
+        <div className='w-4/5 max-h-md overflow-y-auto scroll-auto mb-10  grid grid-cols-1 lg:grid-cols-2 gap-10 p-10'>
+        <ProjectBox
+          img = {fuzzychat}
+          name={'FuzzyChat'}
+          description={"A chat-app with basic implementation of socket.io"}
+          tools={["React, Express", "Postgresql", "Socket.io", "Prisma"]}
+        />
+       
+        <ProjectBox
+          img = {fuzzychat}
+          name={'FuzzyChat'}
+          description={"A chat-app with basic implementation of socket.io"}
+          tools={["React, Express", "Postgresql", "Socket.io", "Prisma"]}
+        />
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
+  </div>
   )
 }
 

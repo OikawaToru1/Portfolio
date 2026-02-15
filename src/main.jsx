bb328f6bf5408ca97ae5a87a1281fbefae24a7dd
Aniket <aniketadhikari2@gmail.com>
2026-02-15 18:34:24 +0545

    changes in react roouter implementaion

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './outlet/Outlet.jsx'
+import {About,Contact} from './Pages/index.js'
+
+let routers = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout/>,
+    children : [
+      {
+        index : true,
+        Component : App
+      },
+      {
+        path : "about",
+        Component : About
+      },
+      {
+        path : 'contact',
+        Component : Contact
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </StrictMode>,
+  
 )

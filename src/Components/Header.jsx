a02f1f6c2db8038845f37304c72809ab09e8c361
Aniket <aniketadhikari2@gmail.com>
2026-02-15 18:32:25 +0545

    Header for the page

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='h-1/5 bg-blue-500 w-full'>
+      <h1>
+        Aniket dai
+      </h1>
+    </div>
+  )
+}
+
+export default Header

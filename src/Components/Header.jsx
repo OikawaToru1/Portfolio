0f8ba27e0537b1a0edfd07d30aa49f66c06f0450
Aniket <aniketadhikari2@gmail.com>
2026-02-17 11:17:04 +0545

    added sidebar feature for mobile devices

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,14 +6,30 @@ import { changeTheme } from '../store/themeSlice.js';
 import { Link } from 'react-router';
 
 
+
 function Header() {
   const darkTheme = useSelector((state) => state.theme.darkTheme);
+
   const dispatch = useDispatch();
   const toggleTheme = ()=>{
     dispatch(changeTheme());
   }
 
-  console.log(darkTheme);
+  const navLinks = [
+    {
+      field : "About",
+      link : "/about",
+    },
+    {
+      field : "Projects",
+      link : "/projects",
+    },
+    {
+      field : "Contact",
+      link : "/contact",
+    },
+  ]
+
 
 
   return (
@@ -21,7 +37,14 @@ function Header() {
       <h1 className='text-3xl px-2 font-bold text-blue-500 '>
         <Link to="/">Aniket</Link>
       </h1>
-      <div className='pr-12'>
+      <div className='md:pr-12 pr-6  flex gap-5 items-center'>
+        <div className='flex gap-4 visible md:invisible '>
+          {navLinks.map((link) => (
+            <Link key={link.field} to={link.link} className="hover:text-blue-500 font-semibold transition-colors duration-300">
+              {link.field}
+            </Link>
+          ))}
+        </div>
         {darkTheme ? <FaMoon onClick={toggleTheme} className='text-2xl text-yellow-400 hover:text-yellow-300 cursor-pointer ' />:
          <FiSun onClick={toggleTheme} className='text-2xl text-gray-500 hover:text-gray-700 cursor-pointer ' />}
       </div>

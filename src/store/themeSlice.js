6a9d9c175406cbbfba3113367fab7cb2530e2ede
Aniket <aniketadhikari2@gmail.com>
2026-02-17 11:03:21 +0545

    created store and slice for dark theme

---
+++ src/store/themeSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    darkTheme : false,
+}
+
+export const themeSlice = createSlice({
+    name : "darkTheme",
+    initialState,
+    reducers : {
+        changeTheme : (state)=>{
+            state.darkTheme = !state.darkTheme;
+        }
+    }
+})
+
+export const {changeTheme} = themeSlice.actions;
+
+export default themeSlice.reducer;
